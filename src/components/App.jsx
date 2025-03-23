1d158207dcc6ea66648ff06051c879aabea707e7
Antonio Marmolejo <djcamg71@gmail.com>
2025-03-23 01:09:13 -0500

    feat: copia del contenido del proyecto 11 de react

---
+++ src/components/App.jsx
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from "react";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
+import CurrentUserContext from "../contexts/CurrentUserContext";
+import { api } from "../utils/Api";
+
+export default function App() {
+    const [currentUser, setCurrentUser] = useState({});
+    const [cards, setCards] = useState([]);
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [popupType, setPopupType] = useState(null);
+    const [selectedCardToDelete, setSelectedCardToDelete] = useState(null);
+
+
+    useEffect(() => {
+        api.getUserInfo().then(setCurrentUser).catch(console.error);
+        api.getInitialCard().then(setCards).catch(console.error);
+    }, []);
+
+    //Función para contralar el boton, de reciclaje, al momento de darle click al ícono de basura
+    function handleRecycleClick(card) {
+        setSelectedCardToDelete(card);
+        handleOpenPopup("deleteCard");
+    }
+
+    //Lógica de los likes y dislikes de cada tarjeta
+    const handleCardLike = async (card) => {
+        const isLiked = card.likes.some((user) => user._id === currentUser._id);
+        const promise = isLiked ? api.deleteCardLink(card._id) : api.linkCard(card._id);
+
+        try {
+            const newCard = await promise;
+            setCards((prevCards) => prevCards.map((c) => (c._id === card._id ? newCard : c)));
+        } catch (error) {
+            console.error("Error al manejar el like:", error);
+        }
+    };
+
+    const handleAddPlaceSubmit = async (data) => {
+        try {
+            const newCard = await api.addCard(data.name, data.link);
+            setCards([newCard, ...cards]); // Añadir la nueva tarjeta al inicio
+            handleClosePopup();
+        } catch (error) {
+            console.error("Error al agregar la nueva tarjeta:", error);
+        }
+    };
+
+    const handleCardDelete = async (evt) => {
+        evt.preventDefault();
+        try {
+            await api.deleteCard(selectedCardToDelete._id);
+            setCards((prevCards) => prevCards.filter((c) => c._id !== selectedCardToDelete._id));
+            handleClosePopup();
+        } catch (error) {
+            console.error("Error al eliminar la tarjeta:", error);
+        }
+    };
+
+    const handleOpenPopup = (type) => {
+        setIsPopupOpen(true);
+        setPopupType(type);
+    };
+    const handleClosePopup = () => {
+        setIsPopupOpen(false);
+        setPopupType(null);
+        setSelectedCardToDelete(null);
+    };
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const data = await api.getUserInfo();
+                setCurrentUser(data);
+            } catch (error) {
+                console.error("Error al obtener la información del usuario:", error);
+            }
+        })();
+    }, []);
+
+    const handleUpdateUser = async (data) => {
+        try {
+            const newDate = await api.updateUser(data.name, data.about);
+            setCurrentUser(newDate); //Actualizar los datos del perifl de usuario en currentUser
+        } catch (error) {
+            console.error("Error al actualizar el usuario", error);
+        }
+    };
+
+    async function handleUpdaterAvatar(data) {
+        if (!data.avatar) {
+            console.error("No se ha proporcionado una URL de avatar válida");
+            return;
+        }
+
+        try {
+            const updateUserAvatar = await api.updateUserPhoto(data.avatar); //Actulizar la imagen del ávatar
+            setCurrentUser(updateUserAvatar);
+            handleClosePopup();
+        } catch (error) {
+            console.error("Error al Actualizar el Avatar: " + error);
+        }
+    }
+
+    return (
+        <CurrentUserContext.Provider value={{ currentUser }}>
+            <div className="page">
+                <main className="main">
+                    <Header />
+                    <Main
+                        cards={cards}
+                        onCardLike={handleCardLike}
+                        onCardDelete={handleCardDelete}
+                        onAddPlaceSubmit={handleAddPlaceSubmit}
+                        onRecycleClick={handleRecycleClick}
+                        onOpenPopup={(type) => {
+                            handleOpenPopup(type);
+                        }}
+                        onClosePopup={() => {
+                            handleClosePopup();
+                        }}
+                        isPopupOpen={isPopupOpen}
+                        popupType={popupType}
+                        onUpdateAvatar={handleUpdaterAvatar}
+                        onUpdateUser={handleUpdateUser}
+                    />
+                    <Footer />
+                </main>
+            </div>
+        </CurrentUserContext.Provider>
+    );
+}
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useContext } from "react";
+import pencil from "../../images/Vector-pencil2.svg";
+import imagenSuma from "../../images/Vector-suma-agregar.svg";
+import Popup from "./components/Popup/Popup";
+import EditProfile from "./components/Popup/EditProfile/EditProfile";
+import Avatar from "./components/Popup/EditAvatar/EditAvatar";
+import NewCard from "./components/Popup/NewCard/NewCard";
+import Card from "./components/Card/Card";
+import ImagePopup from "./components/Popup/ImagePopup/ImagePopup";
+import DeleteCard from "./components/Popup/RemoveCard/RemoveCard";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+
+export default function Main({
+    onOpenPopup,
+    onClosePopup,
+    isPopupOpen,
+    popupType,
+    onUpdateAvatar,
+    cards,
+    onCardLike,
+    onCardDelete,
+    onRecycleClick,
+    onAddPlaceSubmit,
+    onUpdateUser,
+}) {
+    const [selectedImage, setSelectedImage] = useState(null);
+    const { currentUser } = useContext(CurrentUserContext);
+
+
+    return (
+        <>
+            <div className="buttons">
+                <div className="buttons buttons__avatar">
+                    <button className="buttons__edit" onClick={() => onOpenPopup("changeAvatar")}>
+                        <img className="buttom__edit_pencil" src={pencil} alt="icono de pencil" />
+                    </button>
+                    <img className="buttons__image" src={currentUser.avatar} alt="Imagen del Avatar" />
+                </div>
+
+                <div className="buttons buttons__intro">
+                    <h2 className="buttons__intro buttons__name">{currentUser.name}</h2>
+
+                    <button className="buttons__item buttons__item_index_profile" onClick={() => onOpenPopup("editProfile")}>
+                        <img src={pencil} alt="icono de pencil" />
+                    </button>
+                    <p className="buttons__intro buttons__explorer">{currentUser.about}</p>
+                </div>
+
+                <button className="buttons__item buttons__item_index_add-card" onClick={() => onOpenPopup("newPlace")}>
+                    <img src={imagenSuma} alt="ícono de mas para agregar tarjetas" />
+                </button>
+
+                <div className="buttons__modal"></div>
+            </div>
+
+            {selectedImage && <ImagePopup card={selectedImage} onClose={() => setSelectedImage(null)} />}
+
+            <div className="cards">
+                {cards.map((card) => (
+                    <Card
+                        key={card._id}
+                        card={card}
+                        onCardLike={onCardLike}
+                        onRecycleClick={() => onRecycleClick(card)}
+                        onImagenClick={setSelectedImage}
+                    />
+                ))}
+            </div>
+
+            {isPopupOpen && (
+                <Popup isOpen={isPopupOpen} onClose={onClosePopup}>
+                    {popupType === "editProfile" && (
+                        <EditProfile
+                            onUpdateUser={onUpdateUser}
+                            onClose={onClosePopup}
+                        />)}
+                    {popupType === "newPlace" && (
+                        <NewCard
+                            onAddPlaceSubmit={onAddPlaceSubmit}
+                            onClose={onClosePopup} />)
+                    }
+                    {popupType === "changeAvatar" && <Avatar onUpdateAvatar={onUpdateAvatar} onClose={onClosePopup} />}
+                    {popupType === "deleteCard" && (
+                        <DeleteCard
+                            onConfirm={onCardDelete}
+                            onClose={onClosePopup}
+                        />
+                    )}
+                </Popup>
+            )}
+        </>
+    );
+}
