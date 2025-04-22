3e78b5dd73d1536b1bec4178a5954c953aefd1ca
Antonio Marmolejo <djcamg71@gmail.com>
2025-04-22 01:56:04 -0500

    feat: cración del componente infoTooltip que muestra un modal utilizando el popup

--- src/components/infoTooltip/InfoTooltip.jsx
+++ src/components/infoTooltip/InfoTooltip.jsx
@@ -1,1 +1,19 @@
-import "../infoTooltip/InfoTooltip.css";
+import React from "react";
+import Popup from "../Main/components/Popup/Popup";
+import successIcon from "../../images/success.svg";
+import errorIcon from "../../images/error.svg";
+
+export default function InfoTooltip({ isOpen, onClose, message, isError }) {
+    return (
+        <Popup isOpen={isOpen} onClose={onClose}>
+            <div className="info-tooltip">
+                <img
+                    src={isError ? errorIcon : successIcon}
+                    alt={isError ? "Error" : "Éxito"}
+                    className="info-tooltip__icon"
+                />
+                <p className="info-tooltip__message">{message}</p>
+            </div>
+        </Popup>
+    );
+}
